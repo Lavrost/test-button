9e759cb3724872bbe29863a09987b6e3a45f67bc
Rostislav Lavrinenko <lav.rost@yandex.ru>
2024-02-16 11:41:27 +0300

    Добавил ф-ию addButton

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-console.log('Hello, world!');
-
 const btn_submit = document.getElementById("submit");
+const content_result = document.getElementById("content-result");
+console.log(document.btn_submit);
 
+function update(item) {
+    item.className = "";
+}
 
-function func1() {
+function func1() { // old createButton
     const btn_result = document.getElementById("button-result");
-    function update(item) {
-        item.className = "";
-    }
     update(btn_result);
-    console.log("It's work!");
     const color = document.getElementById("bg-color");
     const size = document.getElementById("size");
     const text = document.getElementById("content-text");
     btn_result.classList.add(color.value, size.value);
     btn_result.textContent = text.value;
-    // console.log(color.value);
 }
+
+function addButton(color, size, text) { // color, size - имена классов; text - текст кнопки
+    const button = document.createElement('button');
+    button.classList.add('btn');
+    button.textContent = text;
+    button.classList.add(color, size);
+    document.content_result.appendChild(button);
+}
+
+// addButton('red', 'small', 'Кнопка');
+// addButton('grey', 'big', 'Кнопка');
